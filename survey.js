10429574615aa81c17c28a2d09a3ed4280b2f85e
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-01-14 23:37:41 +0000

    multiply question worked on with @matthew-charles-chan and @rainskyriver took about 30mins

---
+++ survey.js
@@ -0,0 +1,15 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question('What’s your name? ', (name) => {
+  rl.question('What’s an activity you like? ', (activity) => {
+    rl.question('What’s your favourite band? ', (band) => {
+      rl.question('What’s your favourite sport? ', (sport) => {
+        console.log(`My name is ${name} and I like ${activity}. When playing ${sport}, I listen to ${band}.`);
+        rl.close();
+      });
+    });
+  });
+});
